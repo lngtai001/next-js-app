6304f3057ec808ba1d065a330aaa5f25c818678a
Taiani Lange <Taiani.lange@vastech.co.za>
2023-09-07 10:56:37 +0200

    get tailwind styles working in scss module

---
+++ src/app/button.tsx
@@ -0,0 +1,11 @@
+
+import BtnStyles from "./button.module.scss"
+  
+export default function Something() {
+    return (
+        <div className="min-h-screen flex flex-row justify-center items-center gap-4">
+            <button className={BtnStyles.primary}>Primary</button>
+            <button className={BtnStyles.error}>Error</button>
+        </div>
+    );
+}
