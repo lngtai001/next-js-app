1939a4368820300e281297b02a2da146211d21b0
Taiani Lange <Taiani.lange@vastech.co.za>
2023-09-07 10:44:31 +0200

    add sass, and init variable file to test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+ 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
+
+
+
