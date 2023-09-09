3f599865a607bd1c96c649a6d2fde6f3cf81f7b5
Taiani Lange <Taiani.lange@vastech.co.za>
2023-09-09 14:44:54 +0200

    add contact us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
       <SomeButtons /> */}
 
       { navigation.map((item) => (
-          <MyCustomSection key={item.name} id={item.id} className={' h-[calc(100vh-74px)] w-screen bg-gray-200 pt-20'}>
+          <MyCustomSection key={item.name} id={item.id} className={' h-[calc(100vh-74px)] w-screen pt-20'}>
             {item.name}
             {componentMap.get(item.name)}
           </MyCustomSection>
@@ -27,7 +27,7 @@ export default function Page() {
       <About />
       <Services /> */}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -95,7 +95,7 @@ export default function Page() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   )
 }
