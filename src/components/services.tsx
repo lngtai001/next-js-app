33c558be905e1f8c9a12c8053c420c308be5ba6f
Taiani Lange <Taiani.lange@vastech.co.za>
2023-09-09 15:17:44 +0200

    Add some stats and services

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,7 +1,110 @@
+interface Stat {
+    viewId: number;
+    amount: string;
+    title: string;
+}
+
+const stats: Stat[] = [
+    { viewId: 0, amount: "2500", title: "Happy Clients"},
+    { viewId: 1, amount: "300", title: "Full Notebooks"},
+    { viewId: 2, amount: "120", title: "Teammates"},
+    { viewId: 3, amount: "30", title: "Stores"},
+]
+
+const Statistics = () => {
+    return (
+        <>
+        { stats.map((item) => (
+            <div key={item.viewId} className="p-4 sm:w-1/4 w-1/2">
+                <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">{item.amount}</h2>
+                <p className="leading-relaxed">{item.title}</p>
+            </div>
+        ))}
+        </>
+    )
+}
+
+export function StatisticsContainer(){
+    return (
+        <div className="text-gray-600 body-font bg-pink-200 py-10">
+            <div className="container px-5 py-2 mx-auto">
+                <div className="flex flex-wrap -m-4 text-center">
+                    <Statistics />
+                </div>
+            </div>
+        </div>
+    )
+}
+
 export function Services(){
     return (
-    <div >
-        This is SERVICES component
+        <>
+        <StatisticsContainer />
+<div className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="text-center mb-20">
+      <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Services</h1>
+      <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug.</p>
+      <div className="flex mt-6 justify-center">
+        <div className="w-16 h-1 rounded-full bg-pink-500 inline-flex"></div>
+      </div>
     </div>
+    <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+      <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+        <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-pink-100 text-pink-500 mb-5 flex-shrink-0">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10" viewBox="0 0 24 24">
+            <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+          </svg>
+        </div>
+        <div className="flex-grow">
+          <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
+          <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+          <a className="mt-3 text-pink-500 inline-flex items-center">Learn More
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+        <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-pink-100 text-pink-500 mb-5 flex-shrink-0">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10" viewBox="0 0 24 24">
+            <circle cx="6" cy="6" r="3"></circle>
+            <circle cx="6" cy="18" r="3"></circle>
+            <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+          </svg>
+        </div>
+        <div className="flex-grow">
+          <h2 className="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
+          <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+          <a className="mt-3 text-pink-500 inline-flex items-center">Learn More
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+        <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-pink-100 text-pink-500 mb-5 flex-shrink-0">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10" viewBox="0 0 24 24">
+            <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+            <circle cx="12" cy="7" r="4"></circle>
+          </svg>
+        </div>
+        <div className="flex-grow">
+          <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
+          <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+          <a className="mt-3 text-pink-500 inline-flex items-center">Learn More
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+    <button className="flex mx-auto mt-16 text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg">Button</button>
+  </div>
+</div>
+</>
     );
 };
