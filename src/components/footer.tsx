bbdb5cb10ddc50f3a8a6706b9464d7db65bf0505
Taiani Lange <Taiani.lange@vastech.co.za>
2023-09-11 10:04:26 +0200

    use react-icons instead of svgs

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,54 +1,6 @@
 import Logo from "@/components/logo";
 import Link from "next/link";
-
-// TODO TL move SVGs to public folder
-function FacebookSVG() {
-    return (
-        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-        </svg>
-    )
-}
-
-function TwitterSVG() {
-    return (
-        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-        </svg>
-    )
-}
-
-function InstagramSVG() {
-    return (
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-        </svg>
-    )
-}
-
-function LinkedInSVG() {
-    return (
-        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
-                <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-                <circle cx="4" cy="4" r="2" stroke="none"></circle>
-        </svg>
-    )
-}
-
-const svgArray = [FacebookSVG, TwitterSVG ,InstagramSVG, LinkedInSVG]
-
-const SocialMedia = () => {
-    return (
-        <>
-        { svgArray.map((item, index) => (
-            <Link key={index} className="ml-3 text-gray-500" href={""}>
-                { item() }
-            </Link>
-        ))}
-        </>
-    )
-}
+import { FaFacebook, FaFacebookF, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 function Bottom() {
     return (
@@ -56,8 +8,11 @@ function Bottom() {
             <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
                 <Logo />
                 <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2019 Acme — All rights reserved.</p>
-                <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                <SocialMedia />
+                <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 gap-3 justify-center sm:justify-start">
+                <FaFacebookF className="w-5 h-5 opacity-70" />
+                <FaTwitter className="w-5 h-5 opacity-70" />
+                <FaInstagram className="w-5 h-5 opacity-70" />
+                <FaLinkedin className="w-5 h-5 opacity-70" />
                 </span>
             </div>
         </div>
@@ -70,10 +25,10 @@ export default function Footer() {
           <div className="container px-5 py-24 mx-auto">
             <div className="flex flex-wrap md:text-left text-center order-first">
               <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CONTENT</h2>
                 <nav className="list-none mb-10">
                   <li>
-                    <Link className="text-gray-600 hover:text-gray-800" href={""}>First Link</Link>
+                    <Link className="text-gray-600 hover:text-gray-800" href="">First Link</Link>
                   </li>
                   <li>
                     <a className="text-gray-600 hover:text-gray-800">Second Link</a>
@@ -87,24 +42,7 @@ export default function Footer() {
                 </nav>
               </div>
               <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-                <nav className="list-none mb-10">
-                  <li>
-                    <a className="text-gray-600 hover:text-gray-800">First Link</a>
-                  </li>
-                  <li>
-                    <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-                  </li>
-                  <li>
-                    <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-                  </li>
-                  <li>
-                    <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-                  </li>
-                </nav>
-              </div>
-              <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">INFORMATION</h2>
                 <nav className="list-none mb-10">
                   <li>
                     <a className="text-gray-600 hover:text-gray-800">First Link</a>
@@ -121,12 +59,29 @@ export default function Footer() {
                 </nav>
               </div>
               <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUBSCRIBE</h2>
+                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">LEGAL</h2>
+                <nav className="list-none mb-10">
+                  <li>
+                    <a className="text-gray-600 hover:text-gray-800">First Link</a>
+                  </li>
+                  <li>
+                    <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+                  </li>
+                  <li>
+                    <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+                  </li>
+                  <li>
+                    <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                  </li>
+                </nav>
+              </div>
+              <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                {/* <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUBSCRIBE</h2> */}
                 <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
-                  <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+                  {/* <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2"> */}
                     {/* <label for="footer-field" className="leading-7 text-sm text-gray-600">Placeholder</label>
                     <input type="text" id="footer-field" name="footer-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-pink-200 focus:border-pink-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"> */}
-                  </div>
+                  {/* </div> */}
                   <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">Button</button>
                 </div>
                 <p className="text-gray-500 text-sm mt-2 md:text-left text-center">Bitters chicharrones fanny pack
